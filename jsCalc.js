c0223752ec5f1ca235d78c820a52613a434c44f2
Graham Williams <willigra@dickinson.edu>
2014-12-18 23:37:31 -0500

    Made the textbox and slider correspond to eachother

--- jsCalc.js
+++ jsCalc.js
@@ -1,1 +1,19 @@
-document.getElementById("title").style.color = "blue";
+document.getElementById("desired").addEventListener("blur", textChange);
+document.getElementById("slider").addEventListener("change", slideChange);
+document.getElementById("add").addEventListener("click", addRow);
+
+function textChange() {
+    var newDog = document.getElementById("desired").value;
+    document.getElementById("slider").value = newDog;
+}
+
+function slideChange() {
+    var newDog = document.getElementById("slider").value;
+    document.getElementById("desired").value = newDog;
+}
+
+function addRow() {
+    var tableList = document.getElementsByTagName("tr");
+    var numRows = tableList.length - 1;
+    console.log("number of rows: " + numRows);
+}
