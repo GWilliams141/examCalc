6c707c4a764d89eb2c9559b78eee551556f5e190
Graham Williams <willigra@dickinson.edu>
2014-12-29 15:09:22 -0500

    Add row button now functional

--- jsCalc.js
+++ jsCalc.js
@@ -16,4 +16,30 @@ function addRow() {
     var tableList = document.getElementsByTagName("tr");
     var numRows = tableList.length - 1;
     console.log("number of rows: " + numRows);
+
+    var newRow = document.createElement("tr");
+
+    var newCell1 = document.createElement("td");
+    var newCell2 = document.createElement("td");
+    var newCell3 = document.createElement("td");
+
+    var newTextbox1 = document.createElement("input");
+    //input.name would assign it an id? I think...
+    var newTextbox2 = document.createElement("input");
+    var newTextbox3 = document.createElement("input");
+    // Why can't I use the same percent node for both boxes?
+    var percent1 = document.createTextNode("%");
+    var percent2 = document.createTextNode("%");
+
+    newCell1.appendChild(newTextbox1);
+    newCell2.appendChild(newTextbox2);
+    newCell2.appendChild(percent1);
+    newCell3.appendChild(newTextbox3);
+    newCell3.appendChild(percent2);
+
+    newRow.appendChild(newCell1);
+    newRow.appendChild(newCell2);
+    newRow.appendChild(newCell3);
+
+    document.getElementById("table").appendChild(newRow);
 }
